6c2b6aea4a63f3c0fca490d9478e7daec65e60bb
Benjamin Gfr <benjamin.gfroerer@gmx.de>
2025-12-14 13:43:29 +0100

    UnitHealth cleaned up

--- BangerProjekt/Assets/Scripts/CombatScripts/PlayerScripts/Player.cs
+++ BangerProjekt/Assets/Scripts/CombatScripts/PlayerScripts/Player.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public UnitHealth PlayerHealth = new UnitHealth(100, 100); //Player starts with 100HP (just example, can be changed anytime)
+    public UnitHealth PlayerHealth = new UnitHealth(100); //Player starts with 100HP (just example, can be changed anytime)
 
 
 
