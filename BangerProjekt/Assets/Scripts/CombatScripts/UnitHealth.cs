e91118c1297be5ab458964dd08a1dda961dfa05a
Benjamin Gfr <benjamin.gfroerer@gmx.de>
2025-12-14 13:43:29 +0100

    UnitHealth cleaned up

--- BangerProjekt/Assets/Scripts/CombatScripts/UnitHealth.cs
+++ BangerProjekt/Assets/Scripts/CombatScripts/UnitHealth.cs
@@ -7,11 +7,11 @@ using UnityEngine.UIElements;
 public class UnitHealth //we do not inherit from "MonoBehaivour" here, so this is just a plain C# Class
 {
     private int currentHealth;
-    private int currentMaxHealth;
+    private int maxHealth;
     //these are private and should only be touched via the "Health" and "MaxHealth" integers declared below
 
 
-    public int Health //our current health via simple get/set
+    public int CurrentHealth //our current health via simple get/set
     {
         get
         {
@@ -26,20 +26,20 @@ public class UnitHealth //we do not inherit from "MonoBehaivour" here, so this i
     {
         get
         {
-            return currentMaxHealth;
+            return maxHealth;
         }
         set
         {
-            currentMaxHealth = value;
+            maxHealth = value;
         }
     }
 
-    public UnitHealth(int health, int maxHealth) //if we create an Object of class "UnitHealth" we need to set currentHealth and maxHealth
+    public UnitHealth(int health) //if we create an Object of class "UnitHealth" we need to set currentHealth and maxHealth
     {
-        currentHealth = health;
-        currentMaxHealth = maxHealth;
-
+        maxHealth = health;
+        currentHealth = maxHealth;
     }
+
     public void DamageUnit(int damageAmount)
     {
         if (currentHealth > 0)
@@ -53,37 +53,26 @@ public class UnitHealth //we do not inherit from "MonoBehaivour" here, so this i
 
     public void AddMaxHealth(int healthToAdd)
     {
-        currentMaxHealth = healthToAdd + currentMaxHealth; //add to maxHealth
-        MaxHealth = currentMaxHealth; //set the MaxHealth
+        maxHealth = healthToAdd + maxHealth; //add to maxHealth
         if (healthToAdd > 0)
         {
             currentHealth += healthToAdd; //currentHP also increase when maxHP increase. This does NOT happen when maxHP decrease
         }
-            if (currentHealth > currentMaxHealth)
-            {
-                currentHealth = currentMaxHealth;
+        if (currentHealth > maxHealth)
+        {
+                currentHealth = maxHealth;
             //afterwards check if currHP are higher than maxHP (especially for MaxHP DECREASES)
-            }
+        }
         
 
     }
     public void HealUnit(int healAmount)
     {
-        if (currentHealth < currentMaxHealth)
+        if (currentHealth+healAmount< maxHealth)//Checking if there is Overheal before healing
         {
             currentHealth += healAmount;
         }
-        if (currentHealth > currentMaxHealth)
-        {
-            currentHealth = currentMaxHealth;
-        }
-        //i dont even need to explain this
+        else currentHealth = maxHealth; 
+        
     }
-
-
-
-
-
-
-
 }
