cc748f4b52bd934b55c6046ab30361a122587b36
Dead0Night <ramizahmed4123@gmail.com>
2025-12-14 09:29:46 +0100

    Added TitleScreen and MenuButton
    * Titlescreen
    
    As simple as it gets, just "Start", "Options", and "Quit" (Options do nothing atm)
    
    * UI Option, Titelscreen, Scripts
    
    One Script that loads our scenes via a "SceneManager" and another one that handles everything in the TitleScreen
    Also there is a Button in the "Game" Scene now that brings you back to the Main Menu
    
    Co-authored-by: Krzzt

---
+++ BangerProjekt/Assets/Scripts/Scene&ScreenScripts/SceneLoader.cs
@@ -0,0 +1,31 @@
+//UnityEngine.SceneManagement is it needed to load scenes??
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneLoader : MonoBehaviour
+{
+    //Name of the scene that should be loaded if the button is pressed
+    public string sceneToLoad="Game";
+
+    public void LoadScene()
+    {
+        if (string.IsNullOrEmpty(sceneToLoad))
+        {
+            Debug.LogError("Scene to load is not set!");
+            return;
+        }
+
+        //Load the specified scene
+        UnityEngine.SceneManagement.SceneManager.LoadScene(sceneToLoad);
+    }
+    
+    // Quit the application
+    public void QuitGame()
+    {
+        //End the application
+        Application.Quit();
+        Debug.Log("SceneLoader: Game is exiting"); //Only visible in the editor
+    }
+   
+}
