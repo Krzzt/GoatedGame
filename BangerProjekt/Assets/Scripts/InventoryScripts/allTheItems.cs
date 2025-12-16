3f85bf94f1963c9a669c0d1c93986f15098006f7
KingHD <77498297+KingHD234@users.noreply.github.com>
2025-12-16 15:10:00 +0100

    a Few basic changes
    - just changing everything again again

--- BangerProjekt/Assets/Scripts/InventoryScripts/allTheItems.cs
+++ BangerProjekt/Assets/Scripts/InventoryScripts/allTheItems.cs
@@ -1,33 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
-public static class ItemList
 
-{
-    public static ItemList<Item> allItems = new List<Item>
-    {
-        new Armour {ID = 0, itemName="Rusty Helmet", itemCategory = "armour" , itemtag = slotTag.head  ,description = "some rust in helmet shape", itemRarity= item.rarity.Dev, defense=0.1 , healthBonus=0}
 
-    };
-}
-public enum slotTag
-{
-    none,
-    head,
-    chest,
-    legs,
-    feet
-}
-public class Item // Creating a item class for the standart item
-{
 
-    public enum itemCategory
-    {
-        weapon,
-        armour
-    }   // Item Categories include armour, weapons
+[CreateAssetMenu]
+public class Item : ScriptableObject // Creating a item class for the standart item
+{   //weapon 
+    public float fireRate;
 
+    public float damage;
+    //armour
+    public float defense;
+
+    public float healthBonus;
+    //item
     public int ID;
     public slotTag itemtag;
     public string itemName;
@@ -47,27 +37,5 @@ public class Item // Creating a item class for the standart item
 
     public rarity itemRarity;
     public string description;
-    
-
-}
-
-public class Armour : item // creating a armour class with the required information for armour
-{
-    
-    public float defense;
-
-    public float healthBonus;
-
-}
-
-
-
- public class Weapon : item // creating the weapon class withe the required information about a weapon
-{
-    
-    public float fireRate;
-
-    public float damage;
-    
 
 }
