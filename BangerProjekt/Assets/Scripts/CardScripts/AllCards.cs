1de8b7d50e1f7d1baffde58e50365767bef75d52
Dead0Night <ramizahmed4123@gmail.com>
2026-02-22 13:53:52 +0100

    repair x2

---
+++ BangerProjekt/Assets/Scripts/CardScripts/AllCards.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AllCards : MonoBehaviour
+{
+   [field:SerializeField] public List<Card> Cards { get; set; }
+}
