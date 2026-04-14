c88d5c1a288d4ce3035443c4efe0fd90c574a5fb
TLgaming <q.kostikovgmail.com>
2026-04-14 11:27:06 +0200

    Added Dragging but not dropping

--- BangerProjekt/Assets/Scripts/UIScripts/InventoryScripts/ItemInSlot.cs
+++ BangerProjekt/Assets/Scripts/UIScripts/InventoryScripts/ItemInSlot.cs
@@ -1,18 +1,51 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
-public class ItemInSlot : MonoBehaviour
+public class ItemInSlot : MonoBehaviour, IBeginDragHandler, IEndDragHandler, IDragHandler, IPointerClickHandler
 {
-    // Start is called before the first frame update
-    void Start()
+    public Item item;
+    private Image image;
+    private RectTransform rectTransform;
+    private Transform parentAfterDrag;
+    private CanvasGroup canvasGroup;
+    [field:SerializeField] private Canvas canvas;
+
+    void Awake()
     {
-        
+        image = GetComponent<Image>();
+        rectTransform = this.GetComponent<RectTransform>();
+        canvasGroup = this.GetComponent<CanvasGroup>();
+        canvas = GetComponentInParent<InventoryScript>().Canvas;
+    }
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        Debug.Log("BeginDrag");
+        parentAfterDrag = transform.parent;
+        transform.SetParent(transform.root);
+        transform.SetAsLastSibling();
+        canvasGroup.blocksRaycasts = false;
+        canvasGroup.alpha = 0.7f;
+    }
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        Debug.Log("EndDrag");
+        canvasGroup.blocksRaycasts = true;
+        canvasGroup.alpha = 1f;
+    }
+    public void OnDrag(PointerEventData eventData)
+    {
+        Debug.Log("OnDrag");
+         rectTransform.anchoredPosition += eventData.delta / canvas.scaleFactor;
+         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	public void OnPointerClick(PointerEventData eventData)
+	{
+		Debug.Log("Clicked");
+	}
 }
